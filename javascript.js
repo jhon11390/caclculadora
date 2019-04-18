99a996702c529a1b2f53fb937f75eded522e73a1
jhon11390 <meljhon2@gmail.com>
2019-04-18 14:54:36 -0500

    se crea funciones para sola operacion basica y se reducela letra de la pantalla

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let pressnumber = [];
+let numerosparaoperar =[];
 
 function addpantalla(numero){
     pressnumber.push(numero);
@@ -10,10 +11,13 @@ function addpantalla(numero){
         document.querySelector('.resultados').innerHTML = "";
         document.querySelector('.resultados').append(dato);
     }
+    numerosparaoperar.push(dato);
+    console.log(numerosparaoperar);
 }
 
 function resetearpantalla(){
-    pressnumber = [];
+    numerosparaoperar.splice(0, numerosparaoperar.length);
+    pressnumber.splice(0, pressnumber.length);
     document.querySelector('.resultados').innerHTML = "";
 }
 
@@ -21,4 +25,43 @@ function operadores(operador){
     pressnumber = [];
     document.querySelector('.resultados').innerHTML = "";
     document.querySelector('.resultados').append(operador);
+    numerosparaoperar.push(operador);
+}
+
+function resultadototal(){
+    numerosparaoperar.forEach(element => {
+        if(element == '+'){
+            let dato1 = parseInt(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
+            let dato2 = parseInt(numerosparaoperar[numerosparaoperar.length-1]);
+            let total = dato1 + dato2;
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append(total);
+        } else if(element == '-'){
+            let dato1 = parseInt(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
+            let dato2 = parseInt(numerosparaoperar[numerosparaoperar.length-1]);
+            let total = dato1 - dato2;
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append(total);
+        } else if(element == 'x'){
+            let dato1 = parseInt(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
+            let dato2 = parseInt(numerosparaoperar[numerosparaoperar.length-1]);
+            let total = dato1 * dato2;
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append(total);
+        } else if(element == '/'){
+            let dato1 = parseInt(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
+            let dato2 = parseInt(numerosparaoperar[numerosparaoperar.length-1]);
+            let total = dato1 / dato2;
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append(total);
+        } else if(element == "√"){
+            let dato1 = parseInt(numerosparaoperar[numerosparaoperar.length-1]);
+            let total = Math.sqrt(dato1);
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append(total);
+        } else if(element == '%'){
+            document.querySelector('.resultados').innerHTML = "";
+            document.querySelector('.resultados').append('DISABLED');
+        }
+    });
 }
