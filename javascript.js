1c410991bf7d8b1e05f94d12ee9da858c9242fce
jhon11390 <meljhon2@gmail.com>
2019-04-25 12:09:33 -0500

    se añaden funciones y queda pendiente raiza y porcentaje

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@
 let pressnumber = [];
 let numerosparaoperar =[];
 let operacionfinal = [];
+let resultados = [];
+let simbolos = document.querySelectorAll('.simbolos');
+let numeros = document.querySelectorAll('.unidad');
 
 function addpantalla(numero){
     pressnumber.push(numero);
@@ -13,14 +16,37 @@ function addpantalla(numero){
         document.querySelector('.resultados').append(dato);
     }
     numerosparaoperar.push(dato);
+    
     console.log(numerosparaoperar);
 }
 
 function resetearpantalla(){
     numerosparaoperar.splice(0, numerosparaoperar.length);
     pressnumber.splice(0, pressnumber.length);
+    operacionfinal.splice(0, operacionfinal.length);
+    resultados.splice(0, resultados.length);
     document.querySelector('.resultados').innerHTML = "";
+    simbolos[0].setAttribute('onclick', 'operadores("%")');
+    simbolos[1].setAttribute('onclick', 'operadores("√")');
+    simbolos[2].setAttribute('onclick', 'operadores("+")');
+    simbolos[3].setAttribute('onclick', 'operadores("-")');
+    simbolos[4].setAttribute('onclick', 'operadores("x")');
+    simbolos[5].setAttribute('onclick', 'operadores("/")');
+    simbolos[6].setAttribute('onclick', 'resultadototal()');
+    numeros[0].setAttribute('onclick', 'addpantalla("9")');
+    numeros[1].setAttribute('onclick', 'addpantalla("8")');
+    numeros[2].setAttribute('onclick', 'addpantalla("7")');
+    numeros[3].setAttribute('onclick', 'addpantalla("6")');
+    numeros[4].setAttribute('onclick', 'addpantalla("5")');
+    numeros[5].setAttribute('onclick', 'addpantalla("4")');
+    numeros[6].setAttribute('onclick', 'addpantalla("3")');
+    numeros[7].setAttribute('onclick', 'addpantalla("2")');
+    numeros[8].setAttribute('onclick', 'addpantalla("1")');
+    numeros[9].setAttribute('onclick', 'addpantalla("0")');
+    numeros[10].setAttribute('onclick', 'addpantalla(".")');
+
 }
+
 
 function operadores(operador){
     pressnumber = [];
@@ -51,21 +77,25 @@ function resultadototal(){
             operacionfinal.push(Math.sqrt(numerosparaoperar[numerosparaoperar.indexOf(element)+1]));
             numerosparaoperar.splice(numerosparaoperar.indexOf(element),1,'raiz');
         } else if(element == '%'){
-            numerosparaoperar.forEach(element => {
-                if(element == 'x'){
-                    let dato1 = parseInt(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
-                    let dato2 = parseInt(numerosparaoperar[numerosparaoperar.length-2]);
-                    let total = dato1 * dato2/100;
-                    console.log(dato2);
-                    document.querySelector('.resultados').innerHTML = "";
-                    document.querySelector('.resultados').append(total);
-                }
-            });
+            operacionfinal.push(numerosparaoperar[numerosparaoperar.indexOf(element)-1]);
+            operacionfinal.push('%');
+            numerosparaoperar.splice(numerosparaoperar.indexOf(element),1,'porcentaje');
         } 
     });
-    operacionfinal.push(numerosparaoperar[numerosparaoperar.length-1])
+    console.log(operacionfinal);
+    console.log(numerosparaoperar);
+    operacionfinal.push(numerosparaoperar[numerosparaoperar.length-1]);
     let total = operacionfinal.join().replace(/,/g, "");
+    resultados.push(eval(total));
+    console.log(resultados);
     console.log(total);
     document.querySelector('.resultados').innerHTML = "";
     document.querySelector('.resultados').append(eval(total));
+    simbolos.forEach(element => {
+        element.setAttribute('onclick', null);
+    });
+    numeros.forEach(element => {
+        element.setAttribute('onclick', null);
+    });
+    numeros[11].setAttribute('onclick', 'resetearpantalla()')
 }
